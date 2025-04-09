b4ed512ba2a35be3e19623a495f3d22654217af6
TL <tlgamer.gaming@gmail.com>
2025-04-09 18:18:18 +0300

    koko sovellus nettiin

--- part3/puhback/index.js
+++ part3/puhback/index.js
@@ -5,7 +5,7 @@ app.use(express.json())
 const cors = require('cors')
 
 app.use(cors())
-
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 let persons = [
