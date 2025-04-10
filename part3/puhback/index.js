00eda96fa2426260d13dabaad0571c0a880f88b9
TL <tlgamer.gaming@gmail.com>
2025-04-10 19:28:53 +0300

    uibuild

--- part3/puhback/index.js
+++ part3/puhback/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 app.use(express.json())
 
 
-app.use(cors())
+
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
