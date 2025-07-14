a9a0e81aaaa073b4bbb2bc25b3b97a8d4bb49d8c
TL <tlgamer.gaming@gmail.com>
2025-07-14 17:18:38 +0300

    unohtunut lisätä logger ja middleware sekä nyt myös luotu user luominen

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 const app = express()
 
 
@@ -11,6 +13,9 @@ mongoose.connect(mongoUrl)
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -1,6 +1,8 @@
-const config = require('./utils/config')
 const app =require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
